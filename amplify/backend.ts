dc2d436c485bb8fca00700e96725cf2194acba4d
fraR-coder <francesco.mail5.0@gmail.com>
2024-07-15 16:59:32 +0200

    changed pool

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -3,29 +3,28 @@ import { auth } from "./auth/resource";
 import { data } from "./data/resource";
 
 const backend=defineBackend({
-  // auth,
+   auth,
    data,
 });
 
-console.log('Adding backend output...');
 
-backend.addOutput({
-  auth: {
-    aws_region: "eu-central-1",
-    user_pool_id: "eu-central-1_cxXAfMLPD",
-    user_pool_client_id: "hjo60fv8sri9rq8j4kokm4c5t",
-    //identity_pool_id: "<your-cognito-identity-pool-id>",
-    username_attributes: ["email"],
-    standard_required_attributes: ["email"],
-    user_verification_types: ["email"],
+// backend.addOutput({
+//   auth: {
+//     aws_region: "eu-central-1",
+//     user_pool_id: "eu-central-1_cxXAfMLPD",
+//     user_pool_client_id: "hjo60fv8sri9rq8j4kokm4c5t",
+//     //identity_pool_id: "<your-cognito-identity-pool-id>",
+//     username_attributes: ["email"],
+//     standard_required_attributes: ["email"],
+//     user_verification_types: ["email"],
     
-    unauthenticated_identities_enabled: true,
-    // password_policy: {
-    //   min_length: 8,
-    //   require_lowercase: true,
-    //   require_uppercase: true,
-    //   require_numbers: true,
-    //   require_symbols: true,
-    // },
-  },
-});
+//     unauthenticated_identities_enabled: true,
+//     // password_policy: {
+//     //   min_length: 8,
+//     //   require_lowercase: true,
+//     //   require_uppercase: true,
+//     //   require_numbers: true,
+//     //   require_symbols: true,
+//     // },
+//   },
+// });
